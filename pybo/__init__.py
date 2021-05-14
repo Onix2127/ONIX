3ada0507ed2dd5a643847771cd2f73896218242b
ONIX <onix2127@gmail.com>
2021-05-14 14:54:36 +0900

    pybo.py 추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -7,31 +7,27 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 # --------------------------------------------------------------------------- #
-
-
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 # --------------------------------- [edit] ---------------------------------- #
-    app.config.from_object(config)
+app.config.from_object(config)
 
-    # ORM
-    db.init_app(app)
-    migrate.init_app(app, db)
+# ORM
+db.init_app(app)
+migrate.init_app(app, db)
 # --------------------------------- [edit] ---------------------------------- #
-    from . import models
+from . import models
 # --------------------------------------------------------------------------- #
 
-    # 블루프린트
+# 블루프린트
 # --------------------------------------------------------------------------- #
-    from .views import main_views, question_views, answer_views, auth_views, naver_views, movie_views
-    app.register_blueprint(main_views.bp)
-    app.register_blueprint(question_views.bp)
-    app.register_blueprint(answer_views.bp)
-    app.register_blueprint(auth_views.bp)
-    app.register_blueprint(naver_views.bp)
-    app.register_blueprint(movie_views.bp)
+from .views import main_views, question_views, answer_views, auth_views, naver_views, movie_views
+app.register_blueprint(main_views.bp)
+app.register_blueprint(question_views.bp)
+app.register_blueprint(answer_views.bp)
+app.register_blueprint(auth_views.bp)
+app.register_blueprint(naver_views.bp)
+app.register_blueprint(movie_views.bp)
 
-    from .filter import format_datetime
-    app.jinja_env.filters['datetime'] = format_datetime
+from .filter import format_datetime
+app.jinja_env.filters['datetime'] = format_datetime
 
-    return app
